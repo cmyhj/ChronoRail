0ee6f8feb727cea6f576da6b01b418ce5613d57f
cmyhj <autism2484684043@163.com>
2026-08-12 22:01:31 +0800

    fix: random page content hidden behind shader bg (z-0 -> -z-10)

--- src/components/Common/HeroShader.tsx
+++ src/components/Common/HeroShader.tsx
@@ -2,7 +2,7 @@ import { MeshGradient } from "@paper-design/shaders-react";
 
 export function ShaderBackground() {
   return (
-    <div className="fixed inset-0 w-full h-full overflow-hidden pointer-events-none z-0">
+    <div className="fixed inset-0 w-full h-full overflow-hidden pointer-events-none -z-10">
       <MeshGradient
         className="absolute inset-0 w-full h-full"
         colors={["#0d1428", "#16224e", "#232a6e", "#3a2578", "#552070", "#6e1f58", "#7a2430", "#1a3a3a"]}
