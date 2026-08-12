3258da5740cc5e6dc5e2c0a8cf837ec58bd0b645
cmyhj <autism2484684043@163.com>
2026-08-12 21:41:14 +0800

    style: widen MeshGradient shader color contrast (5 purple -> 8 spread hues)

--- src/components/Common/HeroShader.tsx
+++ src/components/Common/HeroShader.tsx
@@ -5,7 +5,7 @@ export function ShaderBackground() {
     <div className="fixed inset-0 w-full h-full overflow-hidden pointer-events-none z-0">
       <MeshGradient
         className="absolute inset-0 w-full h-full"
-        colors={["#14142e", "#2d2a5e", "#1a1a40", "#2f2360", "#1c1c3a"]}
+        colors={["#0d1428", "#16224e", "#232a6e", "#3a2578", "#552070", "#6e1f58", "#7a2430", "#1a3a3a"]}
         speed={0.5}
         distortion={0.6}
         swirl={0.25}
